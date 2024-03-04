20a23e688bbd907400da794a745283cd2e2122cb
Sruthi Kilari <sruthikilari@microsoft.com>
2024-03-04 14:12:09 -0800

    remove comments

--- Utils/scripts/Terraform/resources/main.tf
+++ Utils/scripts/Terraform/resources/main.tf
@@ -132,17 +132,7 @@ resource "azurerm_key_vault" "kv" {
   depends_on = [azurerm_resource_group.rg]
 
   sku_name = "standard"
-
-  # access_policy {
-  #   tenant_id = data.azurerm_client_config.current.tenant_id
-  #   object_id = data.azurerm_client_config.current.object_id #user
-
-  #   secret_permissions = [
-  #     "Get",
-  #     "Set",
-  #     "List"
-  #   ]
-  # }
+  
 }
 
 
